8db5bf9f5d05cead20ad63b910f9a5d70c714d12
CubaneroBlanco <cubanero@hotmail.com>
2025-06-01 14:24:31 -0400

    Initial commit of social network DApp

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,22 @@
-require('babel-register');
-require('babel-polyfill');
-
 module.exports = {
+  contracts_directory: "./src/contracts",
+  contracts_build_directory: "./src/abis",
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Ganache GUI or CLI port
+      network_id: "*"        // Match any network id
+    }
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+      version: "0.5.0",      // Match your contract pragma
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
       }
     }
   }
-}
+};
